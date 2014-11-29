1b00428722f958ff3d08c2f5649b90cd5ddce4f5
Max Losch <mmlosch@kth.se>
2014-11-29 16:14:46 +0100

    visit nodes only onces.

--- navigation/include/navigation/Graph.h
+++ navigation/include/navigation/Graph.h
@@ -224,6 +224,10 @@ void Graph::path_to_next_unknown(int id_from, std::vector<int>& path)
         int id = queue.front();
         queue.pop();
 
+        //check if node already visited
+        if (previous[id] != -1)
+            continue;
+
         navigation_msgs::Node& node = _nodes[id];
         if (node.id_north >= 0) {
             if(update_dijkstra(id, node.id_north, _nodes, previous, distances))
