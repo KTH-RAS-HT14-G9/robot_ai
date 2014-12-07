761801e5fcc510a76598042f20813236329e26d3
Max Losch <mmlosch@kth.se>
2014-12-07 19:45:43 +0100

    set merge and dist thresh equal. also update positions

--- navigation/include/navigation/Graph.h
+++ navigation/include/navigation/Graph.h
@@ -83,8 +83,8 @@ protected:
 Graph::Graph()
     :_next_node_id(0)
     ,_merge_thresh("/navigation/graph/merge_thresh",robot::dim::wheel_distance/1.5)
-    ,_dist_thresh("/navigation/graph/dist_thresh",robot::dim::wheel_distance)
-    ,_update_positions("/navigation/graph/update_positions",false)
+    ,_dist_thresh("/navigation/graph/dist_thresh",robot::dim::wheel_distance/1.5)
+    ,_update_positions("/navigation/graph/update_positions",true)
 {
 }
 
