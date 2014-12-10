495456e2e8541232fb93ae822411bbafc090bc0b
Lan <lanwang@kth.se>
2014-12-10 17:13:37 +0100

    get the entire path and integrate tsp

--- navigation/src/graph.cpp
+++ navigation/src/graph.cpp
@@ -91,84 +91,16 @@ bool service_place_node(navigation_msgs::PlaceNodeRequest& request,
 
     if (request.object_here == true) {
 
-       if(robotToMapTransform(request.object_x,request.object_y, request.object_x,request.object_y))
+    if(robotToMapTransform(request.object_x,request.object_y, request.object_x,request.object_y))
         {
             _graph.place_object(response.generated_node.id_this, request);
         }
-      else success = false;
+        else success = false;
     }
 
     return success;
 }
 
-void init_path_to_noi(int id_from, int trait) {
-    _path.next = 0;
-    _path.trait = trait;
-    _path.path.clear();
-
-    if (trait == navigation_msgs::NextNodeOfInterestRequest::TRAIT_UNKNOWN_DIR) {
-        ROS_INFO("Finding shortest path to next unkown location...");
-        _graph.path_to_next_unknown(id_from, _path.path);
-    }
-    else if (trait == navigation_msgs::NextNodeOfInterestRequest::TRAIT_OBJECT) {
-        ROS_INFO("Finding shortest path to next object...");
-        _graph.path_to_next_object(id_from, _path.path);
-    }
-    else if (trait == navigation_msgs::NextNodeOfInterestRequest::TRAIT_START)
-    {
-        ROS_INFO("Finding shortest path to start node...");
-        double dummy;
-        _graph.path_to_node(id_from, 0, _path.path,dummy);
-    }
-    else
-        ROS_ERROR("Requested trait %d is not implemented yet.", trait);
-
-//    std::cout << "Path: ";
-//    for(int i = 0; i < _path.path.size(); ++i) {
-//        std::cout << _path.path[i] << " ";
-//    }
-//    std::cout << std::endl;
-}
-
-bool service_next_noi(navigation_msgs::NextNodeOfInterestRequest& request,
-                      navigation_msgs::NextNodeOfInterestResponse& response)
-{
-    if ( request.trait == navigation_msgs::NextNodeOfInterestRequest::TRAIT_UNKNOWN_DIR ||
-         request.trait == navigation_msgs::NextNodeOfInterestRequest::TRAIT_OBJECT ||
-         request.trait == navigation_msgs::NextNodeOfInterestRequest::TRAIT_START )
-    {
-//        //if we haven't finished an existing path: advance a step
-//        if (_path.next < _path.path.size())
-//        {
-//            if (request.trait != _path.trait) {
-//                ROS_ERROR("Trait was changed during path following. Will service request anyway.");
-//                init_path_to_noi(request.id_from, request.trait);
-//            }
-//            if (request.id_from != _path.path[_path.next]) {
-//                ROS_ERROR("Last path was not completed. Will service request anyway.");
-//                init_path_to_noi(request.id_from, request.trait);
-//            }
-//        }
-//        else {
-//            init_path_to_noi(request.id_from, request.trait);
-//        }
-
-        init_path_to_noi(request.id_from, request.trait);
-
-        response.path.path.clear();
-        response.path.path.clear();
-        response.path.path.reserve(_path.path.size());
-        for(int i = 1; i < _path.path.size(); ++i) {
-            response.path.path.push_back(_graph.get_node(_path.path[i]));
-        }
-    }
-    else {
-        ROS_ERROR("Requested trait %d is not implemented yet.", request.trait);
-        return false;
-    }
-
-    return true;
-}
 
 
 
@@ -211,18 +143,24 @@ void generate_all_permutations( std::vector<int>& object_nodes,std::vector<std::
       } while ( std::next_permutation(object_nodes.begin(),object_nodes.end() ));
 }
 
-std::vector<int> find_shortest_path()
+std::vector<int> tsp_traverse_all_objects()
 {
 
     std::vector<int> object_nodes;
     std::vector<int> best_path;
+    std::vector<int> best_objects;
     get_object_node_indexs(object_nodes);
     int perm_num=factorial_cal(object_nodes.size());
-    if (object_nodes.size()==0) {best_path.push_back(_graph.get_node(0).id_this);}
+    if (object_nodes.size()==0)
+    {
+        best_path.push_back(_graph.get_node(0).id_this);
+        return best_path;
+    }
 
     std::vector<std::vector <int> > perm;
     perm.resize(perm_num);
-    best_path.reserve(object_nodes.size()+2);
+    best_objects.reserve(object_nodes.size()+2);
+    best_path.reserve(_graph.num_nodes()*3);
     for (int i=0;i<perm_num;++i)
     {
         perm[i].resize(object_nodes.size()+1); //+1 for starting node
@@ -235,38 +173,126 @@ std::vector<int> find_shortest_path()
     // do iterations for all permutations
     for (int i=0; i<perm_num;++i)
     {
-     double dist_sum=0;
-     for (int j=0; j<object_nodes.size();++j)
-       {
-         _graph.path_to_node(perm[i][j], perm[i][j+1], _path.path,dist);
-         std::cout<<"from "<<perm[i][j]<<"to "<<perm[i][j+1]<<" dist"<<dist<<std::endl;
-        dist_sum=dist_sum+dist;
-       }
+        double dist_sum=0;
+        for (int j=0; j<object_nodes.size();++j)
+        {
+            _graph.path_to_node(perm[i][j], perm[i][j+1], _path.path,dist);
+            std::cout<<"from "<<perm[i][j]<<"to "<<perm[i][j+1]<<" dist"<<dist<<std::endl;
+            dist_sum=dist_sum+dist;
+        }
 
-      _graph.path_to_node(perm[i][object_nodes.size()-1], perm[i][0], _path.path,dist); // for the last object to the strating point
-      std::cout<<"going back dis  "<<dist<< std::endl;
-      dist_sum=dist_sum+dist;
-      std::cout<<dist_sum<<std::endl;
-      if (dist_sum<shortest)
-      {
-          best_id=i;
-          shortest=dist_sum;
-      }
+        _graph.path_to_node(perm[i][object_nodes.size()-1], perm[i][0], _path.path,dist); // for the last object to the strating point
+        std::cout<<"going back dis  "<<dist<< std::endl;
+        dist_sum=dist_sum+dist;
+        std::cout<<dist_sum<<std::endl;
+        if (dist_sum<shortest)
+        {
+            best_id=i;
+            shortest=dist_sum;
+        }
     }
     if (best_id== -1)
     {
         std::cout<<"china"<<std::endl;
-        best_path.push_back(_graph.get_node(0).id_this);
-     }
+        best_objects.push_back(_graph.get_node(0).id_this);
+    }
     for (int i=0; i<object_nodes.size()+1;++i)
     {
-
-        best_path.push_back(perm[best_id][i]);
+        best_objects.push_back(perm[best_id][i]);
     }
-        best_path.push_back(_graph.get_node(0).id_this);
+    best_objects.push_back(_graph.get_node(0).id_this);
+
+    for (int i=0; i<best_objects.size()-1;++i)
+    {
+        std::vector <int > nodes_between;
+        _graph.path_to_node(best_objects[i], best_objects[i+1], nodes_between,dist);
+        for (int j=0; j<nodes_between.size()-1;++j)
+        {
+            best_path.push_back(nodes_between[j]);
+        }
+    }
+    best_path.push_back(_graph.get_node(0).id_this);
     return best_path;
 
 }
+
+void init_path_to_noi(int id_from, int trait) {
+    _path.next = 0;
+    _path.trait = trait;
+    _path.path.clear();
+
+    if (trait == navigation_msgs::NextNodeOfInterestRequest::TRAIT_UNKNOWN_DIR) {
+        ROS_INFO("Finding shortest path to next unkown location...");
+        _graph.path_to_next_unknown(id_from, _path.path);
+    }
+    else if (trait == navigation_msgs::NextNodeOfInterestRequest::TRAIT_OBJECT) {
+        ROS_INFO("Finding shortest path to next object...");
+        _graph.path_to_next_object(id_from, _path.path);
+    }
+    else if (trait == navigation_msgs::NextNodeOfInterestRequest::TRAIT_START)
+    {
+        ROS_INFO("Finding shortest path to start node...");
+        double dummy;
+        _graph.path_to_node(id_from, 0, _path.path,dummy);
+    }
+    else if (trait == navigation_msgs::NextNodeOfInterestRequest::TRAIT_TSP)
+    {
+        ROS_INFO("Finding the shortest path through all objects and go back ");
+
+        _path.path=tsp_traverse_all_objects();
+
+    }
+    else
+        ROS_ERROR("Requested trait %d is not implemented yet.", trait);
+
+//    std::cout << "Path: ";
+//    for(int i = 0; i < _path.path.size(); ++i) {
+//        std::cout << _path.path[i] << " ";
+//    }
+//    std::cout << std::endl;
+}
+
+bool service_next_noi(navigation_msgs::NextNodeOfInterestRequest& request,
+                      navigation_msgs::NextNodeOfInterestResponse& response)
+{
+    if ( request.trait == navigation_msgs::NextNodeOfInterestRequest::TRAIT_UNKNOWN_DIR ||
+         request.trait == navigation_msgs::NextNodeOfInterestRequest::TRAIT_OBJECT ||
+         request.trait == navigation_msgs::NextNodeOfInterestRequest::TRAIT_START ||
+         request.trait == navigation_msgs::NextNodeOfInterestRequest::TRAIT_TSP)
+    {
+//        //if we haven't finished an existing path: advance a step
+//        if (_path.next < _path.path.size())
+//        {
+//            if (request.trait != _path.trait) {
+//                ROS_ERROR("Trait was changed during path following. Will service request anyway.");
+//                init_path_to_noi(request.id_from, request.trait);
+//            }
+//            if (request.id_from != _path.path[_path.next]) {
+//                ROS_ERROR("Last path was not completed. Will service request anyway.");
+//                init_path_to_noi(request.id_from, request.trait);
+//            }
+//        }
+//        else {
+//            init_path_to_noi(request.id_from, request.trait);
+//        }
+
+        init_path_to_noi(request.id_from, request.trait);
+
+        response.path.path.clear();
+        response.path.path.clear();
+        response.path.path.reserve(_path.path.size());
+        for(int i = 1; i < _path.path.size(); ++i) {
+            response.path.path.push_back(_graph.get_node(_path.path[i]));
+        }
+    }
+    else {
+        ROS_ERROR("Requested trait %d is not implemented yet.", request.trait);
+        return false;
+    }
+
+    return true;
+}
+
 void test_request(int id_prev, int dir, bool blocked_n, bool blocked_e, bool blocked_s, bool blocked_w, navigation_msgs::PlaceNodeRequest& request)
 {
     request.id_previous = id_prev;
@@ -312,19 +338,19 @@ void test2_graph_build(std::vector<Point>& points) {
 
     Point p0(0,0);
     Point p1(2,0);
-    //Point pObject1(2,0.5); pObject1.dir = navigation_msgs::Node::OBJECT;
+    Point pObject1(2,0.5); pObject1.dir = navigation_msgs::Node::OBJECT;
     Point p2(2,1);
     Point p3(2,-1);
-    //Point pObject2(2,-1.5); pObject2.dir = navigation_msgs::Node::OBJECT;
+    Point pObject2(2,-1.5); pObject2.dir = navigation_msgs::Node::OBJECT;
 
     linspace(points, p0, p1, navigation_msgs::Node::EAST, 5);
     p1 = points[points.size()-1];
-    //points.push_back(pObject1);
+    points.push_back(pObject1);
     linspace(points, p1, p2, navigation_msgs::Node::NORTH, 5);
     p2 = points[points.size()-1];
     linspace(points, p2, p3, navigation_msgs::Node::SOUTH, 5);
     p3 = points[points.size()-1];
-    //points.push_back(pObject2);
+    points.push_back(pObject2);
 
     _test2_previous_node.id_this = -1;
 }
@@ -474,14 +500,15 @@ int main(int argc, char **argv)
 
     ros::Rate rate(10.0);
    ///////test
-  //  std::vector<Point> test_points;
-  //  test2_graph_build(test_points);
-  //  std::vector<int> best_path;
+   //std::vector<Point> test_points;
+   //test2_graph_build(test_points);
+   //std::vector<int> best_path;
+   //std::cout<<"banana"<<std::endl;
 
 
     while(n.ok())
     {
-        //test2_graph(test_points);
+       // test2_graph(test_points);
 
         float x = _position.x;
         float y = _position.y;
@@ -493,11 +520,11 @@ int main(int argc, char **argv)
 
         _graph_viz->draw();
 
-       // if (_test2_i >= test_points.size()) {
-      //      best_path=find_shortest_path();
-      //       for (int i=0; i<best_path.size();++i){
-      //         std::cout<<best_path[i]<< "->"<<std::endl;
-        //     }
+       //if (_test2_i >= test_points.size()) {
+       //     best_path=tsp_traverse_all_objects();
+       //      for (int i=0; i<best_path.size();++i){
+       //      std::cout<<best_path[i]<< "->"<<std::endl;
+       //     }
 
        // }
 
